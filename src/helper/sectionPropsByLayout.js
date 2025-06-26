ec944f444738904b916e25849897ef58d8dad0f1
mohan-1705 <164631881+mohan-1705@users.noreply.github.com>
2025-06-26 09:35:23 +0530

    Add files via upload

---
+++ src/helper/sectionPropsByLayout.js
@@ -0,0 +1,41 @@
+export const sectionPropsByLayout = {
+  classical: {
+    layout_1: {
+      header: { showProfilePicture: true, showName: true, showContactInfo: true },
+      summary: { showSummary: true },
+      experience: { showExperience: true },
+      education: { showEducation: true },
+      achievement: { showAchievements: true },
+      skills1: { showSkills1: true }
+    },
+    layout_2: {},
+    layout_3: {},
+    layout_4: {},
+    layout_5: {},
+    layout_6: {},
+  },
+  modern: {
+    layout_1: {},
+    layout_2: {},
+    layout_3: {},
+    layout_4: {},
+    layout_5: {},
+    layout_6: {},
+  },
+  simple: {
+    layout_1: {},
+    layout_2: {},
+    layout_3: {},
+    layout_4: {},
+    layout_5: {},
+    layout_6: {},
+  },
+  creative: {
+    layout_1: {},
+    layout_2: {},
+    layout_3: {},
+    layout_4: {},
+    layout_5: {},
+    layout_6: {},
+  }
+}
---
+++ src/helper/stylesByLayout.js
@@ -0,0 +1,107 @@
+
+
+export const stylesByLayout = {
+    classical: {
+        layout_1: {
+            headerStyle: {
+                fontFamily: 'Arial, sans-serif',
+            },
+            subHeaderStyle: {
+                fontSize: 20,
+                fontWeight: 'normal',
+                color: '#333',
+                marginBottom: 8,
+            },
+            subSubHeaderStyle: {
+                fontSize: 18,
+                fontWeight: 'normal',
+                color: '#555',
+                marginBottom: 6,
+            },
+            normalStyle: {
+                fontSize: 16,
+                color: '#666',
+            },
+            nameStyle: {
+                fontSize: 22,
+                fontWeight: 'bold',
+                color: '#000',
+            }
+        },
+        layout_2: {
+
+        },
+        layout_3: {
+
+        },
+        layout_4: {
+
+        },
+        layout_5: {
+
+        },
+        layout_6: {
+
+        }
+    },
+    modern: {
+        layout_1: {
+
+        },
+        layout_2: {
+
+        },
+        layout_3: {
+
+        },
+        layout_4: {
+
+        },
+        layout_5: {
+
+        },
+        layout_6: {
+
+        }
+    },
+    simple: {
+        layout_1: {
+
+        },
+        layout_2: {
+
+        },
+        layout_3: {
+
+        },
+        layout_4: {
+
+        },
+        layout_5: {
+
+        },
+        layout_6: {
+
+        }
+    },
+    creative: {
+        layout_1: {
+        },
+        layout_2: {
+
+        },
+        layout_3: {
+
+        },
+        layout_4: {
+
+        },
+        layout_5: {
+
+        },
+
+        layout_6: {
+
+        }
+    }
+}
